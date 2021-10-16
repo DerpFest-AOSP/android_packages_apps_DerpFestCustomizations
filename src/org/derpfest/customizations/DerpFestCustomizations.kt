07ee1f4f33ec56e94048b8361844bd6c7587e5ff
Elluzion <dyrex2004@gmail.com>
2021-10-16 21:11:44 +0200

    DerpFestCustomizations: Initialise
    This empty Customizations app is written in Kotlin
    and acts as an extension of packages/apps/Settings.
    
    DerpFest edits:
    - Includes https://github.com/Project-Fluid/vendor_fextras/blob/fluid-12.1/prebuilt/FluidUI/FluidUI.aar
      (reverse engineered and rewritten in kotlin)
    - Refactor CardPreference and CardSwitchPreference to handle null attributes and improve layout handling
    - Replace RoundedImageView with our logo
    - SwitchPreference -> SwitchPreferenceCompat
    - Small code improvements here and there
    
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Change-Id: I859a4d5c25b07bcf45f998e0ad54a4d9afca11ec
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

---
+++ src/org/derpfest/customizations/DerpFestCustomizations.kt
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: Project Fluid
+ * SPDX-FileCopyrightText: DerpFest AOSP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.customizations
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent
+
+import android.os.Bundle
+
+import androidx.preference.Preference
+import androidx.appcompat.app.ActionBar
+
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+
+class DerpFestCustomizations : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.home)
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        return true
+    }
+
+    override fun getMetricsCategory(): Int = MetricsEvent.DERPFEST
+
+    companion object {
+        const val TAG = "DerpFestCustomizations"
+    }
+}
