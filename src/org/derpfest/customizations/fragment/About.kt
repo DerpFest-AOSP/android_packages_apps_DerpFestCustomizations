e4d5b12317b4f539897e5afdc6fae0623ff1169c
Ido Ben-Hur <idoybh2@gmail.com>
2025-10-12 19:36:33 +0200

    DerpFestCustomizations: Add device specific links support
    Maintainers should overlay over `about_device_fw_ver` `about_device_fw_link` `about_device_recovery_ver` and `about_device_recovery_link`
    * Strings contain no value by default and will not show unless overlayed
    * Version strings should contain a version name that'll be shown in the preference summary (optional)
    * Link strings should contain full links - recommend using direct download links
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/derpfest/customizations/fragment/About.kt
+++ src/org/derpfest/customizations/fragment/About.kt
@@ -9,13 +9,40 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent
 
 import android.os.Bundle
 
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+
 import com.android.settings.R
 import com.android.settings.SettingsPreferenceFragment
 
 class About : SettingsPreferenceFragment() {
 
+    private var mDeviceLinks: PreferenceCategory? = null
+    private var mDeviceFW: Preference? = null
+    private var mDeviceRecovery: Preference? = null
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.about)
+
+        val res = resources
+
+        mDeviceLinks = findPreference("about_device_links")
+        mDeviceFW = findPreference("device_fw")
+        mDeviceRecovery = findPreference("device_recovery")
+
+        val hasFWLink = res.getString(R.string.about_device_fw_link).isNotEmpty()
+        if (!hasFWLink) {
+            mDeviceFW?.isVisible = false
+        }
+
+        val hasRecoveryLink = res.getString(R.string.about_device_recovery_link).isNotEmpty()
+        if (!hasRecoveryLink) {
+            mDeviceRecovery?.isVisible = false
+        }
+
+        if (!hasFWLink && !hasRecoveryLink) {
+            mDeviceLinks?.isVisible = false
+        }
     }
 
     override fun getMetricsCategory(): Int = MetricsEvent.DERPFEST
