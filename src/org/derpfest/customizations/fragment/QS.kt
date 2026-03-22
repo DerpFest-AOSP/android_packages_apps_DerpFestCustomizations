7dfeccf0dad3d4e15f9631405e103213dfdff23a
Tim Zimmermann <tim@linux4.de>
2026-03-22 12:41:36 +0100

    DerpFestCustomizations: Add settings to change QS column count [2/2]
    Change-Id: I01ee97fb58f9cf1a0b74f44c1494d68571660ba4
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/derpfest/customizations/fragment/QS.kt
+++ src/org/derpfest/customizations/fragment/QS.kt
@@ -14,6 +14,9 @@ import android.provider.Settings
 
 import androidx.preference.Preference
 import androidx.preference.ListPreference
+import androidx.preference.PreferenceCategory
+
+import org.derpfest.support.preferences.ProperSeekBarPreference
 
 import com.android.settings.R
 import com.android.settings.SettingsPreferenceFragment
@@ -24,6 +27,11 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
     private lateinit var mDataUsageCycleTypePreference: ListPreference
     private var mTileLabelHide: Preference? = null
     private var mTileIconShape: Preference? = null
+    private var mClassicLayoutCategory: PreferenceCategory? = null
+    private lateinit var mQsColumnsClassic: ProperSeekBarPreference
+    private lateinit var mQsColumnsLandscapeClassic: ProperSeekBarPreference
+    private lateinit var mQqsColumnsClassic: ProperSeekBarPreference
+    private lateinit var mQqsColumnsLandscapeClassic: ProperSeekBarPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.qs)
@@ -31,6 +39,19 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
         findPreference<Preference>(KEY_QS_PANEL_STYLE)?.setOnPreferenceChangeListener(this)
         mTileLabelHide = findPreference(KEY_TILE_LABEL_HIDE)
         mTileIconShape = findPreference(KEY_QS_TILE_ICON_SHAPE)
+        mClassicLayoutCategory = findPreference(KEY_CLASSIC_LAYOUT_CATEGORY)
+        mQsColumnsClassic = findPreference(KEY_QS_TILES_COLUMNS_CLASSIC)!!
+        mQsColumnsLandscapeClassic = findPreference(KEY_QS_TILES_COLUMNS_LANDSCAPE_CLASSIC)!!
+        mQqsColumnsClassic = findPreference(KEY_QQS_TILES_COLUMNS_CLASSIC)!!
+        mQqsColumnsLandscapeClassic = findPreference(KEY_QQS_TILES_COLUMNS_LANDSCAPE_CLASSIC)!!
+
+        listOf(
+            mQsColumnsClassic,
+            mQsColumnsLandscapeClassic,
+            mQqsColumnsClassic,
+            mQqsColumnsLandscapeClassic,
+        ).forEach { it.setOnPreferenceChangeListener(this) }
+
         val style = Settings.Secure.getIntForUser(
             requireContext().contentResolver,
             KEY_QS_PANEL_STYLE,
@@ -48,6 +69,7 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        val cr = requireContext().contentResolver
         when (preference.key) {
             KEY_QS_PANEL_STYLE -> {
                 val style = (newValue as? String)?.toIntOrNull() ?: 0
@@ -58,13 +80,78 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
                 updateDataUsageSummary(newValue as? String)
                 return true
             }
+            KEY_QS_TILES_COLUMNS_CLASSIC -> {
+                val v = newValue as? Int ?: return false
+                Settings.System.putInt(cr, SYSTEM_QS_LAYOUT_COLUMNS_CLASSIC, v)
+                mQsColumnsClassic.setValue(v)
+                return true
+            }
+            KEY_QS_TILES_COLUMNS_LANDSCAPE_CLASSIC -> {
+                val v = newValue as? Int ?: return false
+                Settings.System.putInt(cr, SYSTEM_QS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC, v)
+                mQsColumnsLandscapeClassic.setValue(v)
+                return true
+            }
+            KEY_QQS_TILES_COLUMNS_CLASSIC -> {
+                val v = newValue as? Int ?: return false
+                Settings.System.putInt(cr, SYSTEM_QQS_LAYOUT_COLUMNS_CLASSIC, v)
+                mQqsColumnsClassic.setValue(v)
+                return true
+            }
+            KEY_QQS_TILES_COLUMNS_LANDSCAPE_CLASSIC -> {
+                val v = newValue as? Int ?: return false
+                Settings.System.putInt(cr, SYSTEM_QQS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC, v)
+                mQqsColumnsLandscapeClassic.setValue(v)
+                return true
+            }
         }
         return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Settings.Secure.getIntForUser(
+                requireContext().contentResolver,
+                KEY_QS_PANEL_STYLE,
+                0,
+                UserHandle.USER_CURRENT,
+            ) == 1
+        ) {
+            setInitialClassicLayoutValues()
+        }
     }
 
     private fun updateCircularPrefs(circular: Boolean) {
         mTileLabelHide?.isVisible = circular
         mTileIconShape?.isVisible = circular
+        mClassicLayoutCategory?.isVisible = circular
+        if (circular) {
+            setInitialClassicLayoutValues()
+        }
+    }
+
+    private fun setInitialClassicLayoutValues() {
+        val cr = requireContext().contentResolver
+        val qsPort =
+            Settings.System.getInt(cr, SYSTEM_QS_LAYOUT_COLUMNS_CLASSIC, 0).let {
+                if (it > 0) it else DEFAULT_QS_CLASSIC_COLUMNS_PORT
+            }
+        val qsLand =
+            Settings.System.getInt(cr, SYSTEM_QS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC, 0).let {
+                if (it > 0) it else DEFAULT_QS_CLASSIC_COLUMNS_LAND
+            }
+        val qqsPort =
+            Settings.System.getInt(cr, SYSTEM_QQS_LAYOUT_COLUMNS_CLASSIC, 0).let {
+                if (it > 0) it else DEFAULT_QQS_CLASSIC_COLUMNS_PORT
+            }
+        val qqsLand =
+            Settings.System.getInt(cr, SYSTEM_QQS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC, 0).let {
+                if (it > 0) it else DEFAULT_QQS_CLASSIC_COLUMNS_LAND
+            }
+        mQsColumnsClassic.setValue(qsPort)
+        mQsColumnsLandscapeClassic.setValue(qsLand)
+        mQqsColumnsClassic.setValue(qqsPort)
+        mQqsColumnsLandscapeClassic.setValue(qqsLand)
     }
 
     private fun updateDataUsageSummary(cycleTypeValue: String? = null) {
@@ -91,5 +178,25 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
         private const val KEY_QS_PANEL_STYLE = "qs_panel_style"
         private const val KEY_TILE_LABEL_HIDE = "qs_tile_label_hide"
         private const val KEY_QS_TILE_ICON_SHAPE = "qs_tile_icon_shape"
+        private const val KEY_CLASSIC_LAYOUT_CATEGORY = "qs_classic_layout_category"
+        private const val KEY_QS_TILES_COLUMNS_CLASSIC = "qs_tiles_columns_classic"
+        private const val KEY_QS_TILES_COLUMNS_LANDSCAPE_CLASSIC =
+            "qs_tiles_columns_landscape_classic"
+        private const val KEY_QQS_TILES_COLUMNS_CLASSIC = "qqs_tiles_columns_classic"
+        private const val KEY_QQS_TILES_COLUMNS_LANDSCAPE_CLASSIC =
+            "qqs_tiles_columns_landscape_classic"
+
+        /** Matches [android.provider.Settings.System] keys consumed by SystemUI. */
+        private const val SYSTEM_QS_LAYOUT_COLUMNS_CLASSIC = "qs_layout_columns_classic"
+        private const val SYSTEM_QS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC =
+            "qs_layout_columns_landscape_classic"
+        private const val SYSTEM_QQS_LAYOUT_COLUMNS_CLASSIC = "qqs_layout_columns_classic"
+        private const val SYSTEM_QQS_LAYOUT_COLUMNS_LANDSCAPE_CLASSIC =
+            "qqs_layout_columns_landscape_classic"
+
+        private const val DEFAULT_QS_CLASSIC_COLUMNS_PORT = 4
+        private const val DEFAULT_QS_CLASSIC_COLUMNS_LAND = 6
+        private const val DEFAULT_QQS_CLASSIC_COLUMNS_PORT = 4
+        private const val DEFAULT_QQS_CLASSIC_COLUMNS_LAND = 6
     }
 }
