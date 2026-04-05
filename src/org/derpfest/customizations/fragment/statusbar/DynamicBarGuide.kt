da7cf729469ea3f5d7899c9a89298b8bfaa00ab6
NurKeinNeid <nurkeinneid@derpfest.org>
2026-04-05 17:12:09 +0200

    DerpFestCustomizations: Update Dynamic Bar guide to include icons for each step
    -Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/derpfest/customizations/fragment/statusbar/DynamicBarGuide.kt
+++ src/org/derpfest/customizations/fragment/statusbar/DynamicBarGuide.kt
@@ -7,6 +7,7 @@
 package org.derpfest.customizations.fragment.statusbar
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.android.internal.logging.nano.MetricsProto
 import com.android.settings.R
@@ -15,14 +16,35 @@ import com.android.settingslib.widget.LayoutPreference
 
 class DynamicBarGuide : SettingsPreferenceFragment() {
 
-    private data class GuideStep(val titleRes: Int, val bodyRes: Int)
+    private data class GuideStep(val titleRes: Int, val bodyRes: Int, val iconRes: Int)
 
+    /** Matches Axion DynamicBarScreen GUIDE_STEPS icon roles (Material equivalents). */
     private val guideSteps = listOf(
-        GuideStep(R.string.dynamic_bar_guide_chip_title, R.string.dynamic_bar_guide_chip),
-        GuideStep(R.string.dynamic_bar_guide_swipe_title, R.string.dynamic_bar_guide_swipe),
-        GuideStep(R.string.dynamic_bar_guide_expand_title, R.string.dynamic_bar_guide_expand),
-        GuideStep(R.string.dynamic_bar_guide_dismiss_title, R.string.dynamic_bar_guide_dismiss),
-        GuideStep(R.string.dynamic_bar_guide_keyguard_title, R.string.dynamic_bar_guide_keyguard),
+        GuideStep(
+            R.string.dynamic_bar_guide_chip_title,
+            R.string.dynamic_bar_guide_chip,
+            R.drawable.ic_fiber_manual_record,
+        ),
+        GuideStep(
+            R.string.dynamic_bar_guide_swipe_title,
+            R.string.dynamic_bar_guide_swipe,
+            R.drawable.ic_swap_horiz,
+        ),
+        GuideStep(
+            R.string.dynamic_bar_guide_expand_title,
+            R.string.dynamic_bar_guide_expand,
+            R.drawable.ic_touch_app,
+        ),
+        GuideStep(
+            R.string.dynamic_bar_guide_dismiss_title,
+            R.string.dynamic_bar_guide_dismiss,
+            R.drawable.ic_swap_horiz,
+        ),
+        GuideStep(
+            R.string.dynamic_bar_guide_keyguard_title,
+            R.string.dynamic_bar_guide_keyguard,
+            R.drawable.ic_notifications,
+        ),
     )
 
     private val stepViewIds = intArrayOf(
@@ -49,7 +71,7 @@ class DynamicBarGuide : SettingsPreferenceFragment() {
         guideSteps.forEachIndexed { index, step ->
             val stepView = layoutPref.findViewById<android.view.View>(stepViewIds[index])
                 ?: return@forEachIndexed
-            stepView.findViewById<TextView>(R.id.step_number)?.text = "${index + 1}"
+            stepView.findViewById<ImageView>(R.id.step_icon)?.setImageResource(step.iconRes)
             stepView.findViewById<TextView>(R.id.step_title)?.setText(step.titleRes)
             stepView.findViewById<TextView>(R.id.step_body)?.setText(step.bodyRes)
         }
